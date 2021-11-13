a3dc99e1b97a6b2245a45b48fca9c7699af76b6c
Gaabrielf <gsiemiaszko@gmail.com>
2021-11-13 23:35:23 +0100

    Update 7.js

--- Node/Homework_2/7/7.js
+++ Node/Homework_2/7/7.js
@@ -5,6 +5,9 @@ const user = {
 
 const userString = JSON.stringify(user);
 
+// wczytujemy wbudowany moduł 'fs'(file system) i przypisujemy jego wyeksportowane funkcje/zmienne do stałej 'fs'
+// wywołujemy funkcję 'writeFileSync' z naszego modułu 'fs'
+// pierwszy parametr funkcji to nazwa pliku, drugi zaś wartość którą chcemy zapisać
 
 const fs = require('fs');
 fs.writeFileSync('user.json', userString);

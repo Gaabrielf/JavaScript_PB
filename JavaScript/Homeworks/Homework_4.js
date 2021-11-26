6a0dbb2ee1a331d6e054af6bde1e10cb4c777c12
Gaabrielf <gsiemiaszko@gmail.com>
2021-11-26 23:16:45 +0100

    Add files via upload

---
+++ JavaScript/Homeworks/Homework_4.js
@@ -0,0 +1,17 @@
+//Zadanie 1
+
+
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
+// reverseString("Gabriela");
+
+
+//Zadanie 2
+
+// const reversedNum = num => parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num)
+// reversedNum(1200);
+
+//Zadanie 3
+
+// let someObject = require("./Data.json");
